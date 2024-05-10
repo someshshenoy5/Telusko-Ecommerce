899f758ac9504556a4b861de5df91c6803a24326
someshshenoy5 <shenoysomesh5@gmail.com>
2024-05-11 00:01:46 +0530

    Update Product Page Work Done🎉
    i faced a lot of challenges when doing this example i wasnt able to fetch the image directly then during the put function i did a lot of research then  i get to know that it require a seprate key for image and after all this i still wasnt able to send the default image to backend after lot of time i understood that i had to change URL to File. After all this the update product page is working fine

--- ecommerce/src/components/UpdateProduct.jsx
+++ ecommerce/src/components/UpdateProduct.jsx
@@ -5,16 +5,22 @@ import axios from "axios";
 const UpdateProduct = () => {
   const { id } = useParams();
   const [product, setProduct] = useState({});
-  const [image, setImage] = useState(null);
+  const [image, setImage] = useState();
+  // const [imageInfo, setImageInfo] = useState({
+  //   ImageData: null,
+  //   imageName:"",
+  //   imageType:""
+  // })
   const [updateProduct, setUpdateProduct] = useState({
+    id: null,
     name: "",
-    brand: "",
     description: "",
+    brand: "",
     price: "",
     category: "",
-    stockQuantity: "",
     releaseDate: "",
     productAvailable: false,
+    stockQuantity: "",
   });
 
   useEffect(() => {
@@ -23,10 +29,16 @@ const UpdateProduct = () => {
         const response = await axios.get(
           `http://localhost:8080/api/product/${id}`
         );
+
         setProduct(response.data);
-        // console.log(setProduct);
+      
+        const responseImage = await axios.get(
+          `http://localhost:8080/api/product/${id}/image`,
+          { responseType: "blob" }
+        );
+       const imageFile = await converUrlToFile(responseImage.data,response.data.imageName)
+        setImage(imageFile);     
         setUpdateProduct(response.data);
-        setImage(response.data.imageUrl);
       } catch (error) {
         console.error("Error fetching product:", error);
       }
@@ -35,7 +47,18 @@ const UpdateProduct = () => {
     fetchProduct();
   }, [id]);
 
-  const handleSubmit = (e) => {
+  useEffect(() => {
+    console.log("image Updated", image);
+  }, [image]);
+
+
+
+  const converUrlToFile = async(blobData, fileName) => {
+    const file = new File([blobData], fileName, { type: blobData.type });
+    return file;
+  }
+ 
+  const handleSubmit = async(e) => {
     e.preventDefault();
     const updatedProduct = new FormData();
     updatedProduct.append("imageFile", image);
@@ -43,7 +66,8 @@ const UpdateProduct = () => {
       "product",
       new Blob([JSON.stringify(updateProduct)], { type: "application/json" })
     );
-
+  
+  
     axios
       .put(`http://localhost:8080/api/product/${id}`, updatedProduct, {
         headers: {
@@ -56,9 +80,11 @@ const UpdateProduct = () => {
       })
       .catch((error) => {
         console.error("Error updating product:", error);
-        alert("Failed to update product. Please try again."); 
+        console.log("product unsuccessfull update",updateProduct)
+        alert("Failed to update product. Please try again.");
       });
   };
+ 
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -67,141 +93,154 @@ const UpdateProduct = () => {
       [name]: value,
     });
   };
+  
   const handleImageChange = (e) => {
     setImage(e.target.files[0]);
   };
+  
 
   return (
     <div className="update-product-container">
-    <div className="center-container">
-      <h1>Update Product</h1>
-      <form className="row g-3 pt-5" onSubmit={handleSubmit}>
-        <div className="col-md-6">
-          <label className="form-label">
-            <h6>Name</h6>
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            placeholder={product.name}
-            value={updateProduct.name}
-            onChange={handleChange}
-            name="name"
-          />
-        </div>
-        <div className="col-md-6">
-          <label className="form-label">
-            <h6>Brand</h6>
-          </label>
-          <input
-            type="text"
-            name="brand"
-            className="form-control"
-            placeholder={product.brand}
-            value={updateProduct.brand}
-            onChange={handleChange}
-            id="brand"
-          />
-        </div>
-        <div className="col-12">
-          <label className="form-label">
-            <h6>Description</h6>
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            placeholder={product.description}
-            name="description"
-            onChange={handleChange}
-            value={updateProduct.description}
-            id="description"
-          />
-        </div>
-        <div className="col-5">
-          <label className="form-label">
-            <h6>Price</h6>
-          </label>
-          <input
-            type="number"
-            className="form-control"
-            onChange={handleChange}
-            value={updateProduct.price}
-            placeholder={product.price}
-            name="price"
-            id="price"
-          />
-        </div>
-        <div className="col-md-6">
-          <label className="form-label">
-            <h6>Category</h6>
-          </label>
-          <select
-            className="form-select"
-            value={updateProduct.category}
-            onChange={handleChange}
-            name="category"
-            id="category"
-          >
-            <option value="">Select category</option>
-            <option value="laptop">Laptop</option>
-            <option value="headphone">Headphone</option>
-            <option value="mobile">Mobile</option>
-            <option value="electronics">Electronics</option>
-            <option value="toys">Toys</option>
-            <option value="fashion">Fashion</option>
-          </select>
-        </div>
-
-        <div className="col-md-4">
-          <label className="form-label">
-            <h6>Stock Quantity</h6>
-          </label>
-          <input
-            type="number"
-            className="form-control"
-            onChange={handleChange}
-            placeholder={product.stockQuantity}
-            value={updateProduct.stockQuantity}
-            name="stockQuantity"
-            id="stockQuantity"
-          />
-        </div>
-        <div className="col-md-8">
-          <label className="form-label">
-            <h6>Image</h6>
-          </label>
-          <input
-            className="form-control"
-            type="file"
-            onChange={handleImageChange}
-            placeholder="Upload image"
-            name="imageUrl"
-            id="imageUrl"
-          />
-        </div>
-        <div className="col-12">
-          <div className="form-check">
+      <div className="center-container">
+        <h1>Update Product</h1>
+        <form className="row g-3 pt-5" onSubmit={handleSubmit}>
+          <div className="col-md-6">
+            <label className="form-label">
+              <h6>Name</h6>
+            </label>
             <input
-              className="form-check-input"
-              type="checkbox"
-              name="productAvailable"
-              id="gridCheck"
-              checked={product.productAvailable}
-              onChange={(e) =>
-                setProduct({ ...product, productAvailable: e.target.checked })
-              }
+              type="text"
+              className="form-control"
+              placeholder={product.name}
+              value={updateProduct.name}
+              onChange={handleChange}
+              name="name"
             />
-            <label className="form-check-label">Product Available</label>
           </div>
-        </div>
+          <div className="col-md-6">
+            <label className="form-label">
+              <h6>Brand</h6>
+            </label>
+            <input
+              type="text"
+              name="brand"
+              className="form-control"
+              placeholder={product.brand}
+              value={updateProduct.brand}
+              onChange={handleChange}
+              id="brand"
+            />
+          </div>
+          <div className="col-12">
+            <label className="form-label">
+              <h6>Description</h6>
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder={product.description}
+              name="description"
+              onChange={handleChange}
+              value={updateProduct.description}
+              id="description"
+            />
+          </div>
+          <div className="col-5">
+            <label className="form-label">
+              <h6>Price</h6>
+            </label>
+            <input
+              type="number"
+              className="form-control"
+              onChange={handleChange}
+              value={updateProduct.price}
+              placeholder={product.price}
+              name="price"
+              id="price"
+            />
+          </div>
+          <div className="col-md-6">
+            <label className="form-label">
+              <h6>Category</h6>
+            </label>
+            <select
+              className="form-select"
+              value={updateProduct.category}
+              onChange={handleChange}
+              name="category"
+              id="category"
+            >
+              <option value="">Select category</option>
+              <option value="laptop">Laptop</option>
+              <option value="headphone">Headphone</option>
+              <option value="mobile">Mobile</option>
+              <option value="electronics">Electronics</option>
+              <option value="toys">Toys</option>
+              <option value="fashion">Fashion</option>
+            </select>
+          </div>
 
-        <div className="col-12">
-          <button type="submit" className="btn btn-primary">
-            Submit
-          </button>
-        </div>
-      </form>
-    </div>
+          <div className="col-md-4">
+            <label className="form-label">
+              <h6>Stock Quantity</h6>
+            </label>
+            <input
+              type="number"
+              className="form-control"
+              onChange={handleChange}
+              placeholder={product.stockQuantity}
+              value={updateProduct.stockQuantity}
+              name="stockQuantity"
+              id="stockQuantity"
+            />
+          </div>
+          <div className="col-md-8">
+            <label className="form-label">
+              <h6>Image</h6>
+            </label>
+            <img
+              src={image ? URL.createObjectURL(image) : "Image unavailable"}
+              alt={product.imageName}
+              style={{
+                width: "100%",
+                height: "180px",
+                objectFit: "cover",
+                padding: "5px",
+                margin: "0",
+              }}
+            />
+            <input
+              className="form-control"
+              type="file"
+              onChange={handleImageChange}
+              placeholder="Upload image"
+              name="imageUrl"
+              id="imageUrl"
+            />
+          </div>
+          <div className="col-12">
+            <div className="form-check">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                name="productAvailable"
+                id="gridCheck"
+                checked={product.productAvailable}
+                onChange={(e) =>
+                  setProduct({ ...product, productAvailable: e.target.checked })
+                }
+              />
+              <label className="form-check-label">Product Available</label>
+            </div>
+          </div>
+
+          <div className="col-12">
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
